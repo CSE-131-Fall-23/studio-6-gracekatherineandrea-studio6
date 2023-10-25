cfec5c0c65a226aa2e12e54955914b9a537c2408
Andrea <Andrea@Yyt0_0.dhcp.wustl.edu>
2023-10-25 14:22:22 -0500

    studio complete

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -1,81 +1,83 @@
 package studio6;
-
 import edu.princeton.cs.introcs.StdDraw;
-
 public class RecursiveMethods {
-
 	/**
 	 * Computes the geometric sum for the first n terms in the series
-	 * 
+	 *
 	 * @param n the number of terms to include in the sum
 	 * @return the sum of the first n terms of the geometric series (1/2 + 1/4 + 1/8
-	 *         ...)
+	 * ...)
 	 */
 	public static double geometricSum(int n) {
-		if (n==0) {
-			return 0;
-		}
-		else {
-			return Math.pow(0.5, n) + geometricSum (n-1);
-		}
-			// FIXME compute the geometric sum for the first n terms recursively
-			return 0;
+		
+			if(n == 0) {
+				return 0;
+			} else {
+				return (Math.pow(0.5, n) + geometricSum(n-1));
+			}
 		
 	}
-
 	/**
 	 * This method uses recursion to compute the greatest common divisor
 	 * for the two input values
-	 * 
+	 *
 	 * @param p first operand
 	 * @param q second operand
 	 * @return greatest common divisor of p and q
 	 */
 	public static int gcd(int p, int q) {
-		if (p%q==0) {
-			return q;
-		}
-		else { 		
-			return gcd(p,p%q);
-		}	
+		
+			// FIXME compute the gcd of p and q using recursion
+			if (p % q == 0) {
+				return q;
+			} else {
+				return gcd(q, p % q);
+			}
 		
 		
 	}
-
 	
-
 	/**
 	 * This method uses recursion to create a reverse of the given array
-	 * 
+	 *
 	 * @param array the array to create a reverse of, not to be mutated
 	 * @return an array with the same data as the input but it reverse order
 	 */
 	public static int[] toReversed(int[] array) {
-		Reverse(array,0);
-		
-		// FIXME create a helper method that can recursively reverse the given array
-		return new int[0];
+			int i = 0;
+			return toReversedHelper(array, i, array.length - 1 - i);
 	}
 	
-	public static void Reverse(int[] array, int i) {
-		array [i]=array[array.length-i-1];
-		Reverse(array, i++);			
-		
+	public static int[] toReversedHelper(int[] array, int i, int j) {
+		if (i == (array.length - 1)/2) {
+			return array;
+		} else {
+			int temp = array[j];
+			array[j] = array[i];
+			array[i] = temp;
+			i++;
+			return toReversedHelper(array, i, j);
+		}
 	}
-
 	/**
-	 * @param xCenter                       x-coordinate of the center of the circle
-	 *                                      at the current depth
-	 * @param yCenter                       y-coordinate of the center of the circle
-	 *                                      at the current depth
-	 * @param radius                        radius of the circle at the current
-	 *                                      depth
+	 * @param xCenter x-coordinate of the center of the circle
+	 * at the current depth
+	 * @param yCenter y-coordinate of the center of the circle
+	 * at the current depth
+	 * @param radius radius of the circle at the current
+	 * depth
 	 * @param radiusMinimumDrawingThreshold radius above which drawing should occur
 	 */
 	public static void circlesUponCircles(double xCenter, double yCenter, double radius,
 			double radiusMinimumDrawingThreshold) {
-		
-		// FIXME
+			if (radius <= radiusMinimumDrawingThreshold) {
+				return;
+			}
+			StdDraw.circle(xCenter, yCenter, radius);
+			circlesUponCircles(xCenter - radius, yCenter, radius/3, radiusMinimumDrawingThreshold);
+			circlesUponCircles(xCenter, yCenter + radius, radius/3, radiusMinimumDrawingThreshold);
+			circlesUponCircles(xCenter + radius, yCenter, radius/3, radiusMinimumDrawingThreshold);
+			circlesUponCircles(xCenter, yCenter - radius, radius/3, radiusMinimumDrawingThreshold);
+			
 	}
-
 }
